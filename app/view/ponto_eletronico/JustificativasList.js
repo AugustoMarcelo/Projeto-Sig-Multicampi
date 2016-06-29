263c11b153c674e0f088951518fa7eaf88fa4007
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-06-29 10:39:20 -0300

    Consertando problema do grid que não mostra a coluna do id do ponto referenciado

--- app/view/ponto_eletronico/JustificativasList.js
+++ app/view/ponto_eletronico/JustificativasList.js
@@ -6,28 +6,33 @@ Ext.define('Packt.view.ponto_eletronico.JustificativasList', {
     columnLines: true,
     initComponent: function() {
         var me = this;
+        
+        if(Ext.isString(me.store)) {
+			me.store = Ext.create('Packt.store.ponto_eletronico.Justificativas');
+		}
 
         Ext.apply(me, {
             columns: [
-                {
+                {                    
                     text: 'Ponto', 
-                    dataIndex: 'idPonto',
-                    renderer: function(value, metaData, record) {
-                        var store = Ext.getStore('pontoeletronico');
-                        var ponto = store.findRecord('id', value);
-                        if (ponto != null) {
-                            return ponto.get('dataPonto');
+                    dataIndex: 'idPonto'
+                    // renderer: function(value, metaData, record) {
+                    //     console.log(value);
+                    //     var store = Ext.getStore('pontoeletronico');
+                    //     var ponto = store.findRecord('id', value);
+                    //     if (ponto != null) {
+                    //         return ponto.get('dataPonto');
 
-                        } else {
-                            return "Não encontrado.";
-                        }
-                    }
+                    //     } else {
+                    //         return "Não encontrado.";
+                    //     }
+                    // }
                 },
-                {text: 'Entrada/1º Exp.', dataIndex: 'entrada01'},
-                {text: 'Saída/1º Exp.', dataIndex: 'saida01'},
-                {text: 'Entrada/2º Exp.', dataIndex: 'entrada02'},
-                {text: 'Saída/2º Exp.', dataIndex: 'saida02'},
-                {text: 'Data', dataIndex: 'dataJustificativa'}
+                {xtype: 'datecolumn', format: 'H:i:s', text: 'Entrada/1º Exp.', dataIndex: 'entrada01'},
+                {xtype: 'datecolumn', format: 'H:i:s', text: 'Saída/1º Exp.', dataIndex: 'saida01'},
+                {xtype: 'datecolumn', format: 'H:i:s', text: 'Entrada/2º Exp.', dataIndex: 'entrada02'},
+                {xtype: 'datecolumn', format: 'H:i:s', text: 'Saída/2º Exp.', dataIndex: 'saida02'},
+                {xtype: 'datecolumn', format: 'd/m/Y', text: 'Data', dataIndex: 'dataJustificativa'}
             ],
             forceFit: true,                                             //FORÇA AS COLUNAS DO GRID A SE ENCAIXAREM E NÃO CRIAR A BARRA HORIZONTAL NO RODAPÉ DO MESMO
             dockedItems: [
@@ -42,6 +47,6 @@ Ext.define('Packt.view.ponto_eletronico.JustificativasList', {
             ]
         });
         me.callParent(arguments);
-        me.store.load();
+        //me.store.load();
     }
 });
