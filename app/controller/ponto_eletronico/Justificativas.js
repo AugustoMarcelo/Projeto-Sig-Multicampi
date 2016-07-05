7bf77f9e0b3b752aee2573f8996f98c973f113cc
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-07-05 10:29:18 -0300

    Evento do clique em uma célula foi adicionado. Quando o usuário clica em uma célula, o horário informado no momento do ponto é mostrado, facilitando a comparação com o horário da justificativa. No entanto, o mesmo ainda se encontra com problemas, pois o tooltip é mostrado a qualquer momento após o primeiro clique.

--- app/controller/ponto_eletronico/Justificativas.js
+++ app/controller/ponto_eletronico/Justificativas.js
@@ -12,12 +12,56 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
     init: function (application) {
         this.control({
             "justificativaslist": {
-                render: this.onRender
+                render: this.onRender,
+                cellclick: this.onCellClick
             }
         });
     },
 
-    onRender: function (component, options) {        
-        //component.getStore().load();        
+    onRender: function (component, options) {
+        //component.getStore().load();                        
+    },
+
+    onCellClick: function (view, td, cellIndex, record, tr, rowIndex, e, eOpts) {
+        var store = Ext.getStore('pontoeletronico');        
+        if (view.tip) {
+            view.tip.destroy();                        
+            view.tip = null;            
+        }        
+        if(cellIndex > 0 || cellIndex < 5) {
+            view.tip = Ext.create('Ext.tip.ToolTip', {
+                itemId: 'tooltip-horario',
+                autoShow: false,
+                showDelay: 0,                                    
+                stateful: false,
+                target: view.el,
+                width: 100,
+                title: 'Horário original',
+                delegate: view.cellSelector,
+                trackMouse: false,
+                autoHide: true,
+                listeners: {
+                    beforeshow: function (tooltip, eOpts) {
+                        var ponto = store.findRecord('id', record.get('idPonto'));
+                        var horario;
+                        if (cellIndex == 1) {
+                            horario = ponto.get('entrada01');                        
+                        } else if (cellIndex = 2) {
+                            horario = ponto.get('saida01');                        
+                        } else if (cellIndex == 3) {
+                            horario = ponto.get('entrada02');                        
+                        } else if (cellIndex == 4) {
+                            horario = ponto.get('saida02');                        
+                        }
+                        horario = horario != null ? Ext.Date.format(horario, 'H:i:s') : "--:--:--";                    
+                        //tooltip.update(horario);
+                        tooltip.html = horario;                    
+                        // tooltip.on('show', function () {
+                        //     Ext.defer(tooltip.show, 10, tooltip);
+                        // }, tooltip, { single: true });
+                    }                
+                }
+            });
+        }                               
     }
 });
