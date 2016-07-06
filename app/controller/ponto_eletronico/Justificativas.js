32123cd6a8ea67f3c5559a422b28a5442c2ad982
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-07-06 09:06:04 -0300

    Adicionado o evento itemmouseleave para que a tooltip seja destruída sempre que o mouse mudar a sua seleção. Desta forma, a tooltip só aparecerá quando um clique em uma célula for dado e será destruída após isso.

--- app/controller/ponto_eletronico/Justificativas.js
+++ app/controller/ponto_eletronico/Justificativas.js
@@ -13,7 +13,8 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
         this.control({
             "justificativaslist": {
                 render: this.onRender,
-                cellclick: this.onCellClick
+                cellclick: this.onCellClick,
+                itemmouseleave: this.onItemMouseLeave
             }
         });
     },
@@ -22,13 +23,19 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
         //component.getStore().load();                        
     },
 
+    onItemMouseLeave: function (view, record, item, index, e, eOpts) {
+        if (view.tip) {
+            view.tip.destroy();
+        }  
+    },
+
     onCellClick: function (view, td, cellIndex, record, tr, rowIndex, e, eOpts) {
         var store = Ext.getStore('pontoeletronico');        
         if (view.tip) {
             view.tip.destroy();                        
             view.tip = null;            
         }        
-        if(cellIndex > 0 || cellIndex < 5) {
+        if(cellIndex > 0 && cellIndex < 5) {            
             view.tip = Ext.create('Ext.tip.ToolTip', {
                 itemId: 'tooltip-horario',
                 autoShow: false,
@@ -40,28 +47,41 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
                 delegate: view.cellSelector,
                 trackMouse: false,
                 autoHide: true,
-                listeners: {
-                    beforeshow: function (tooltip, eOpts) {
-                        var ponto = store.findRecord('id', record.get('idPonto'));
-                        var horario;
-                        if (cellIndex == 1) {
-                            horario = ponto.get('entrada01');                        
-                        } else if (cellIndex = 2) {
-                            horario = ponto.get('saida01');                        
-                        } else if (cellIndex == 3) {
-                            horario = ponto.get('entrada02');                        
-                        } else if (cellIndex == 4) {
-                            horario = ponto.get('saida02');                        
-                        }
-                        horario = horario != null ? Ext.Date.format(horario, 'H:i:s') : "--:--:--";                    
-                        //tooltip.update(horario);
-                        tooltip.html = horario;                    
-                        // tooltip.on('show', function () {
-                        //     Ext.defer(tooltip.show, 10, tooltip);
-                        // }, tooltip, { single: true });
-                    }                
-                }
+                // listeners: {
+                //     beforeshow: function (tooltip, eOpts) {
+                //         var ponto = store.findRecord('id', record.get('idPonto'));
+                //         var horario;
+                //         if (cellIndex == 1) {
+                //             horario = ponto.get('entrada01');                        
+                //         } else if (cellIndex = 2) {
+                //             horario = ponto.get('saida01');                        
+                //         } else if (cellIndex == 3) {
+                //             horario = ponto.get('entrada02');                        
+                //         } else if (cellIndex == 4) {
+                //             horario = ponto.get('saida02');                        
+                //         }
+                //         horario = horario != null ? Ext.Date.format(horario, 'H:i:s') : "--:--:--";                    
+                //         tooltip.update(horario);                         
+                //         console.log(tooltip.html);                   
+                //         // tooltip.on('show', function () {
+                //         //     Ext.defer(tooltip.show, 10, tooltip);
+                //         // }, tooltip, { single: true });
+                //     }                
+                // }
             });
-        }                               
+            var ponto = store.findRecord('id', record.get('idPonto'));
+            var horario;
+            if (cellIndex == 1) {
+                horario = ponto.get('entrada01');                        
+            } else if (cellIndex = 2) {
+                horario = ponto.get('saida01');                        
+            } else if (cellIndex == 3) {
+                horario = ponto.get('entrada02');                        
+            } else if (cellIndex == 4) {
+                horario = ponto.get('saida02');                        
+            }
+            horario = horario != null ? Ext.Date.format(horario, 'H:i:s') : "--:--:--";                    
+            view.tip.update(horario);                                     
+        }                              
     }
 });
