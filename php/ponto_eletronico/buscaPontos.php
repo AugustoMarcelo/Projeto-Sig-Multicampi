8246f178610c3621373620da67c47e21bd7d0f21
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-07-08 10:23:30 -0300

    Verificação de justificativa para um mesmo ponto.

--- php/ponto_eletronico/buscaPontos.php
+++ php/ponto_eletronico/buscaPontos.php
@@ -6,11 +6,18 @@
     $sql = "SELECT * FROM pontospordia WHERE dataPonto = '$data' AND usuarioId = '$idUserSession'";
     $result = $mysqli->query($sql);
     $resultQuery = $result->fetch_assoc();
+    
+    if ($resultQuery) {
+        $sql = "SELECT j.id FROM justificativa j, pontospordia p WHERE j.idPonto = p.id";
+        $result = $mysqli->query($sql);
+        $resultQueryJustificativas = $result->fetch_assoc();
+    }
 
     echo json_encode(array(
         "success" => $mysqli->connect_errno == 0,
 		"msg" => $mysqli->error,
-        "result" => $resultQuery
+        "ponto" => $resultQuery,
+        "justificativa" => $resultQueryJustificativas
     ));    
 
     $mysqli->close();
