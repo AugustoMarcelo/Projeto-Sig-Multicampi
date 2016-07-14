b8ed09d74daeae2bbfbba6c9c9d376382a5b6124
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-07-14 08:31:46 -0300

    Campo adicionado somente para mostrar o nome do usuário; não será salvo no banco.

--- app/model/ponto_eletronico/PontoEletronico.js
+++ app/model/ponto_eletronico/PontoEletronico.js
@@ -6,6 +6,7 @@ Ext.define('Packt.model.ponto_eletronico.PontoEletronico', {
 	fields: [
 		{name: 'id', type: 'integer'},
 		{name: 'usuarioId', type: 'integer'},
+		{name: 'nomeUsuario', type: 'string', persist: false},				//CAMPO UTILIZADO PARA MOSTRAR NO GRID O NOME DO USUÁRIO AO INVÉS DE BUSCAR NA STORE E EVITAR PROBLEMAS DE NULIDADE 
 		{name: 'dataPonto', type: 'date', dateFormat: 'Y-m-d'},
 		{name: 'ip', type: 'string'},
 		{name: 'entrada01', type: 'date', dateFormat: 'H:i:s'},
