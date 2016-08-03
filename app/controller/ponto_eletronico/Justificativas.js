cf8dcae4b8c55465e8815e7333aeb93ee8206e22
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-08-03 08:39:08 -0300

    Lógica para habilitar o botão de editar; carregar os dados da justificativa no form;

--- app/controller/ponto_eletronico/Justificativas.js
+++ app/controller/ponto_eletronico/Justificativas.js
@@ -9,15 +9,68 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
         'ponto_eletronico.Justificativas'
     ],
 
+    refs: [
+        {
+            ref: 'justificativas',
+            selector: 'justificativas'
+        },
+        {
+            ref: 'justificaPontoForm',
+            selector: 'justificapontoform',
+            autoCreate: true,
+            xtype: 'justificapontoform'
+        }
+    ],
+
     init: function (application) {
         this.control({
             "justificativaslist": {
                 render: this.onRender,
                 cellclick: this.onCellClick,
                 itemmouseleave: this.onItemMouseLeave,
-                itemcontextmenu: this.onRightClickRow
+                itemcontextmenu: this.onRightClickRow,
+                selectionchange: this.onHabilitarBotoes
+            },
+            "justificativas button#edit": {
+                click: this.onEditJustificativa
             }
         });
+    },
+
+    onEditJustificativa: function (button, e, options) {        
+        var grid = button.up('panel').down('grid');
+        var record = grid.getSelectionModel().getSelection();
+        if (record[0]) {
+            var form = this.getJustificaPontoForm();
+            var fieldset = form.down('form').getComponent('fieldset_horarios');
+            var fs_datefield = form.down('form').getComponent('fieldContainer_date');
+            form.setTitle('Editando...');
+            form.down('form').loadRecord(record[0]);            
+            fs_datefield.query('datefield[name=dataPonto]')[0].setValue(record[0].raw.dataPonto);            
+            fieldset.query('textfield#entradaExp1')[0].setValue(record[0].raw.entrada01);            			
+            fieldset.query('textfield#saidaExp1')[0].setValue(record[0].raw.saida01);            
+            fieldset.query('textfield#entradaExp2')[0].setValue(record[0].raw.entrada02);            
+            fieldset.query('textfield#saidaExp2')[0].setValue(record[0].raw.saida02);            
+            for (var i = 0; i < fieldset.query('textfield').length; i++) {                
+                if(fieldset.query('textfield')[i].getValue() != "") {
+                    fieldset.query('checkbox')[i].setValue(true);
+                }
+            }
+        }
+    },
+
+    onHabilitarBotoes: function (model, selected, options) {
+        var grid = this.getJustificativas();
+        var btnEdit = grid.down('#edit');
+        if (selected[0]) {
+            if (btnEdit.isDisabled()) {
+                btnEdit.setDisabled(false);
+            }
+        } else {
+            if (!btnEdit.isDisabled()) {
+                btnEdit.setDisabled(true);
+            }
+        }
     },
 
     onRightClickRow: function (view, record, item, index, e, eOpts) {
@@ -29,15 +82,17 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
                     iconCls: 'verJustificativa',
                     handler: function () {
                         Ext.create('Ext.window.Window', {
-                            title: 'Hello',
+                            title: 'Justificativa',
+                            modal: true,
                             autoShow: true,
                             height: 200,
                             width: 400,
                             layout: 'fit',
                             items: {
-                                xtype: 'text',
-                                padding: 10,                                                                
-                                text: record.get('justificativa')
+                                xtype: 'container',
+                                maxWidth: 400,
+                                margin: 10,
+                                html: record.get('justificativa')
                             }
                         });
                     }
@@ -61,8 +116,8 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
             success: function (conn, response, options, e0pts) {
                 var result = Packt.util.Util.decodeJSON(conn.responseText);
                 if (result.level < 3) {
-                    for (var i = 0; i < component.columns.length; i++) {
-                        component.columns[i].setVisible(true);
+                    for (var i = 0; i < component.columns.length; i++) {                        
+                        component.columns[i].setVisible(true);                        
                     }
                 }
             }
@@ -82,7 +137,7 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
      * EVENTO QUE GERENCIA O CLIQUE EM UMA CÉLULA DO GRID E MOSTRA UMA TOOLTIP AO USUÁRIO
      */
     onCellClick: function (view, td, cellIndex, record, tr, rowIndex, e, eOpts) {
-        var store = Ext.getStore('pontoeletronico');                                    //REFERÊNCIA DA STORE DOS PONTOS
+        var store = Ext.getStore('pontoeletronico');                                    //REFERÊNCIA DA STORE DOS PONTOS                                        
         var cellIndexStart = 0;                                                         //VARIÁVEL QUE DETERMINARÁ A PRIMEIRA CÉLULA QUE CONTÉM HORÁRIO
         var cellIndexEnd = 5;                                                           //VARIÁVEL QUE DETERMINARÁ A ÚLTIMA CÉLULA QUE CONTÉM HORÁRIO
 
@@ -109,19 +164,30 @@ Ext.define('Packt.controller.ponto_eletronico.Justificativas', {
                 trackMouse: false,
                 autoHide: true,
             });
-            var ponto = store.findRecord('id', record.get('idPonto'));
-            var horario;
-            if (cellIndex == (cellIndexStart + 1)) {                                        //VERIFICANDO QUAL O ÍNDICE DA CÉLULA CLICADA PELO USUÁRIO PARA PEGAR O HORÁRIO DO EXPEDIENTE CORRESPONDENTE
-                horario = ponto.get('entrada01');
-            } else if (cellIndex == (cellIndexStart + 2)) {
-                horario = ponto.get('saida01');
-            } else if (cellIndex == (cellIndexStart + 3)) {
-                horario = ponto.get('entrada02');
-            } else if (cellIndex == (cellIndexStart + 4)) {
-                horario = ponto.get('saida02');
-            }
-            horario = horario != null ? Ext.Date.format(horario, 'H:i:s') : "--:--:--";
-            view.tip.update(horario);                                                       //ATUALIZANDO A TOOLTIP COM O HORÁRIO INFORMADO PELO USUÁRIO NO MOMENTO QUE O PONTO FOI BATIDO
+            /**
+             * COMO O MÉTODO LOAD DA STORE É ASSÍNCRONO, OS VALORES DA TOOLTIP SÃO CONFIGURADOS EM SEU CALLBACK
+             */
+            store.load({
+                params: {
+                    limit: 10000
+                },
+                callback: function (records, operation, success) {
+                    var ponto = store.findRecord('id', record.get('idPonto'));
+                    var horario;
+                    if (cellIndex == (cellIndexStart + 1)) {                                        //VERIFICANDO QUAL O ÍNDICE DA CÉLULA CLICADA PELO USUÁRIO PARA PEGAR O HORÁRIO DO EXPEDIENTE CORRESPONDENTE
+                        horario = ponto.get('entrada01');
+                    } else if (cellIndex == (cellIndexStart + 2)) {
+                        horario = ponto.get('saida01');
+                    } else if (cellIndex == (cellIndexStart + 3)) {
+                        horario = ponto.get('entrada02');
+                    } else if (cellIndex == (cellIndexStart + 4)) {
+                        horario = ponto.get('saida02');
+                    }
+                    horario = horario != null ? Ext.Date.format(horario, 'H:i:s') : "--:--:--";
+                    view.tip.update(horario);                                                       //ATUALIZANDO A TOOLTIP COM O HORÁRIO INFORMADO PELO USUÁRIO NO MOMENTO QUE O PONTO FOI BATIDO
+                }
+            });
+
         }
     }
 });
