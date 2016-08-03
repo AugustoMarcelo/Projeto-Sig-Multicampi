05a7c8a95512d9bff1283d962ed9a37901da1232
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-08-03 08:41:19 -0300

    Atributos adicionados ao model apenas para serem mostrados no grid sem precisar usar renderer; os mesmos não serão persistidos no banco

--- app/model/ponto_eletronico/Justificativa.js
+++ app/model/ponto_eletronico/Justificativa.js
@@ -4,13 +4,15 @@ Ext.define('Packt.model.ponto_eletronico.Justificativa', {
     idProperty: 'id',
 
     fields: [
-        {name: 'id', type: 'integer'},                                  //ATRIBUTO IDENTIFICADOR DA JUSTIFICATIVA
-        {name: 'idPonto', type: 'integer'},                             //ATRIBUTO REFERÊNCIA DO PONTO A SER JUSTIFICADO
-        {name: 'justificativa', type: 'string'},                        //JUSTIFICATIVA DO PONTO
-        {name: 'dataJustificativa', type: 'date', dateFormat: 'Y-m-d'}, //DATA DA JUSTIFICATIVA
-        {name: 'entrada01', type: 'date', dateFormat: 'H:i:s'},         //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE ENTRADA NO 1º EXPEDIENTE  
-        {name: 'saida01', type: 'date', dateFormat: 'H:i:s'},           //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE SAIDA NO 1º EXPEDIENTE
-        {name: 'entrada02', type: 'date', dateFormat: 'H:i:s'},         //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE ENTRADA NO 2º EXPEDIENTE
-        {name: 'saida02', type: 'date', dateFormat: 'H:i:s'},           //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE SAIDA NO 2º EXPEDIENTE
+        {name: 'id', type: 'integer'},                                              //ATRIBUTO IDENTIFICADOR DA JUSTIFICATIVA
+        {name: 'nomeUsuario', type: 'string', persist: false},                      //ATRIBUTO UTILIZADO SOMENTE PARA MOSTRAR O NOME DO USUÁRIO NO GRID; NÃO SERÁ PERSISTIDO NO BANCO
+        {name: 'dataPonto', type: 'date', dateFormat: 'Y-m-d', persist: false},     //ATRIBUTO UTILIZADO SOMENTE PARA MOSTRAR A DATA DO PONTO NO GRID; NÃO SERÁ PERSISTIDO NO BANCO
+        {name: 'idPonto', type: 'integer'},                                         //ATRIBUTO REFERÊNCIA DO PONTO A SER JUSTIFICADO
+        {name: 'justificativa', type: 'string'},                                    //JUSTIFICATIVA DO PONTO
+        {name: 'dataJustificativa', type: 'date', dateFormat: 'Y-m-d'},             //DATA DA JUSTIFICATIVA
+        {name: 'entrada01', type: 'date', dateFormat: 'H:i:s'},                     //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE ENTRADA NO 1º EXPEDIENTE  
+        {name: 'saida01', type: 'date', dateFormat: 'H:i:s'},                       //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE SAIDA NO 1º EXPEDIENTE
+        {name: 'entrada02', type: 'date', dateFormat: 'H:i:s'},                     //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE ENTRADA NO 2º EXPEDIENTE
+        {name: 'saida02', type: 'date', dateFormat: 'H:i:s'},                       //ATRIBUTO IDENTIFICADOR DO HORÁRIO DE SAIDA NO 2º EXPEDIENTE
     ]   
 });
