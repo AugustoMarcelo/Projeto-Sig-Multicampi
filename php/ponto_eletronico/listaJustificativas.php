f9194df00df27d089eb4b6705203411edd839c17
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-08-03 08:48:32 -0300

    Modificação na condicional; Um erro estava sendo gerado ao carregar a aplicação

--- php/ponto_eletronico/listaJustificativas.php
+++ php/ponto_eletronico/listaJustificativas.php
@@ -9,16 +9,12 @@
         $idUserLogado = $_SESSION['user_id'];
     }
 
-    if (isset($_SESSION['level'])) {
-        if ($_SESSION['level'] < 3) {
-            $sql = "SELECT j.id, p.dataPonto, j.idPonto, j.justificativa, j.entrada01, j.saida01, j.entrada02, j.saida02, j.dataJustificativa FROM Justificativa j, Pontospordia p GROUP BY j.id DESC LIMIT $start, $limit";    
-
-            $sqlCount = $mysqli->query("SELECT COUNT(*) AS num FROM Justificativa LIMIT $start, $limit");
-        } else {
-            $sql = "SELECT j.id, p.dataPonto, j.idPonto, j.justificativa, j.entrada01, j.saida01, j.entrada02, j.saida02, j.dataJustificativa FROM Justificativa j, Pontospordia p, User u WHERE $idUserLogado = u.id AND p.UsuarioId = u.id AND p.usuarioId = $idUserLogado AND p.id = j.idPonto GROUP BY j.dataJustificativa DESC LIMIT $start, $limit";
-
-            $sqlCount = $mysqli->query("SELECT COUNT(*) AS num FROM Justificativa j, Pontospordia p, User u WHERE $idUserLogado = u.id AND p.UsuarioId = u.id AND j.idPonto = p.id AND p.UsuarioId = $idUserLogado LIMIT $start, $limit");
-        }
+    if (isset($_SESSION['level']) && $_SESSION['level'] < 3) {        
+        $sql = "SELECT j.*, u.name AS nomeUsuario, p.dataPonto FROM Justificativa j, Pontospordia p, User u WHERE j.idPonto = p.id AND p.usuarioId = u.id GROUP BY j.id DESC LIMIT $start, $limit";    
+        $sqlCount = $mysqli->query("SELECT COUNT(*) AS num FROM Justificativa LIMIT $start, $limit");        
+    } else {
+        $sql = "SELECT j.*, p.dataPonto FROM Justificativa j, Pontospordia p, User u WHERE $idUserLogado = u.id AND p.UsuarioId = u.id AND p.usuarioId = $idUserLogado AND p.id = j.idPonto GROUP BY j.dataJustificativa DESC LIMIT $start, $limit";
+        $sqlCount = $mysqli->query("SELECT COUNT(*) AS num FROM Justificativa j, Pontospordia p, User u WHERE $idUserLogado = u.id AND p.UsuarioId = u.id AND j.idPonto = p.id AND p.UsuarioId = $idUserLogado LIMIT $start, $limit");
     }    
 
     $result = array();
