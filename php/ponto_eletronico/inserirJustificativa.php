2308d34289bd8f94f1597fa519292ffda055633d
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-08-17 10:58:49 -0300

    Criando um novo ponto, caso a justificativa seja para um dia faltoso

--- php/ponto_eletronico/inserirJustificativa.php
+++ php/ponto_eletronico/inserirJustificativa.php
@@ -7,7 +7,7 @@
     //RECUPERANDO OS VALORES DO FORMULÁRIO
     $id = $_POST['id'];
     $data = date('Y-m-d');
-    $idPonto = $_POST['idPonto'];                                                                                 //ID DO PONTO REFERENCIADO
+    $idPonto = isset($_POST['idPonto']) ? $_POST['idPonto'] : "";                                                                                 //ID DO PONTO REFERENCIADO
     $entrada01 = isset($_POST['entrada01']) ? $_POST['entrada01'] : false;                                                                           //HORÁRIO DE ENTRADA 1º EXPEDIENTE
     $saida01 = isset($_POST['saida01']) ? $_POST['saida01'] : false;                                                                               //HORÁRIO DE SAÍDA 1º EXPEDIENTE
     $entrada02 = isset($_POST['entrada02']) ? $_POST['entrada02'] : false;                                     //HORÁRIO DE ENTRADA 2º EXPEDIENTE
@@ -16,6 +16,14 @@
     $novo = true;
 
     if ($id == "") {
+        if ($idPonto == "") {
+            $maquinaIp = $_SERVER['REMOTE_ADDR'];
+            $idUsuario = $_SESSION['user_id'];
+            $dataPonto = date('Y-d-m', strtotime($_POST['dataPonto']));
+            $queryInsert = "INSERT INTO pontospordia VALUES (DEFAULT, '$maquinaIp', '$dataPonto', '$idUsuario', null, null, null, null, null)";
+		    $mysqli->query($queryInsert);
+            $idPonto = $mysqli->insert_id;
+        }
         $sql = "INSERT INTO Justificativa VALUES (DEFAULT, '$idPonto', '$justificativa', IF('$entrada01' = '', null, '$entrada01'), IF('$saida01' = '', null, '$saida01'), IF('$entrada02' = '', null, '$entrada02'), IF('$saida02' = '', null, '$saida02'), '$data');";
         $mysqli->query($sql);
     } else {
