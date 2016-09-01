2b63f571737106fa2932b3cfa6768c4a53248f7b
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-09-01 09:58:37 -0300

    O usuário agora pode justificar um dia inteiro

--- php/ponto_eletronico/inserirJustificativa.php
+++ php/ponto_eletronico/inserirJustificativa.php
@@ -13,18 +13,24 @@
     $entrada02 = isset($_POST['entrada02']) ? $_POST['entrada02'] : false;                                     //HORÁRIO DE ENTRADA 2º EXPEDIENTE
     $saida02 = isset($_POST['saida02']) ? $_POST['saida02'] : false;                                           //HORÁRIO DE SAÍDA 2º EXPEDIENTE
     $justificativa = $_POST['justificativa'];                                                            //JUSTIFICATIVA    
+    $checkDayFault = isset($_POST['checkDayFault']) ? true : false;
     $novo = true;
 
     if ($id == "") {
-        if ($idPonto == "") {
+        if ($idPonto == "") {            
             $maquinaIp = $_SERVER['REMOTE_ADDR'];
             $idUsuario = $_SESSION['user_id'];
-            $dataPonto = date('Y-d-m', strtotime($_POST['dataPonto']));
-            $queryInsert = "INSERT INTO pontospordia VALUES (DEFAULT, '$maquinaIp', '$dataPonto', '$idUsuario', null, null, null, null, null)";
+            //$dataPonto = date('Y-m-d', strtotime($_POST['dataPonto']));
+            $dataPonto = $_POST['dataPonto'];            
+            $queryInsert = "INSERT INTO pontospordia VALUES (DEFAULT, '$maquinaIp', '$dataPonto', '$idUsuario', null, null, null, null, null);";
 		    $mysqli->query($queryInsert);
             $idPonto = $mysqli->insert_id;
         }
-        $sql = "INSERT INTO Justificativa VALUES (DEFAULT, '$idPonto', '$justificativa', IF('$entrada01' = '', null, '$entrada01'), IF('$saida01' = '', null, '$saida01'), IF('$entrada02' = '', null, '$entrada02'), IF('$saida02' = '', null, '$saida02'), '$data');";
+        if($checkDayFault == true) {
+            $sql = "INSERT INTO Justificativa VALUES (DEFAULT, '$idPonto', '$justificativa', null, null, null, null, '$data', 1);";
+        } else {
+            $sql = "INSERT INTO Justificativa VALUES (DEFAULT, '$idPonto', '$justificativa', IF('$entrada01' = '', null, '$entrada01'), IF('$saida01' = '', null, '$saida01'), IF('$entrada02' = '', null, '$entrada02'), IF('$saida02' = '', null, '$saida02'), '$data', 0);";
+        }
         $mysqli->query($sql);
     } else {
         $sql = "UPDATE Justificativa SET entrada01 = IF('$entrada01' = '', null, '$entrada01'), saida01 = IF('$saida01' = '', null, '$saida01'), entrada02 = IF('$entrada02' = '', null, '$entrada02'), saida02 = IF('$saida02' = '', null, '$saida02'), justificativa = '$justificativa' WHERE id = '$id';";
