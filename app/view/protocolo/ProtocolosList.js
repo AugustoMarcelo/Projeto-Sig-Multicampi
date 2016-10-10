352729dd87f1e6f045986f0e78b069329f32d70a
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-10-10 07:33:48 -0300

    Customização de grid para representar Empréstimos e Devoluções

--- app/view/protocolo/ProtocolosList.js
+++ app/view/protocolo/ProtocolosList.js
@@ -18,18 +18,105 @@ Ext.define('Packt.view.protocolo.ProtocolosList', {
 
 		Ext.apply(me, {
 			columns: [				
-				{text: 'Movimentação', dataIndex: 'movimentacao', width: '12%'},
-                {text: 'Tombo', dataIndex: 'tombo', width: '10%'},
-                {text: 'Denominação', dataIndex: 'denominacao', width: '20%'},
-                {text: 'Descrição', dataIndex: 'descricao', width: '20%'},
-                {text: 'Mediador', dataIndex: 'mediador', width: '15%'},
-                {text: 'Solicitante', dataIndex: 'solicitante', width: '13%'},
+				{
+					text: 'Movimentação', 
+					dataIndex: 'movimentacao', 
+					width: '11%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;"';
+							return "EMPRÉSTIMO";
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;border-left:4px solid #00B200;"';
+							return "DEVOLUÇÃO";
+						}
+					}
+				},
+                {
+					text: 'Tombo', 
+					dataIndex: 'tombo', 
+					width: '10%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;"';
+							return value;
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;"';
+							return value;
+						}
+					}
+				},
+                {
+					text: 'Denominação', 
+					dataIndex: 'denominacao', 
+					width: '20%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;"';
+							return value;
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;"';
+							return value;
+						}
+					}
+				},
+                {
+					text: 'Especificações', 
+					dataIndex: 'especificacoes', 
+					width: '20%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;"';
+							return value;
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;"';
+							return value;
+						}
+					}
+				},
+                {
+					text: 'Mediador', 
+					dataIndex: 'mediador', 
+					width: '15%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;"';
+							return value;
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;"';
+							return value;
+						}
+					}
+				},
+                {
+					text: 'Solicitante', 
+					dataIndex: 'solicitante', 
+					width: '13%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;"';
+							return value;
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;"';
+							return value;
+						}
+					}
+				},
                 {
                 	xtype: 'datecolumn', 
                 	text: 'Data', 
                 	dataIndex: 'data', 
-                	format: 'd-m-Y H:i:s', 
-                	width: '10%'
+                	format: 'd/m/Y H:i:s', 
+                	width: '11%',
+					renderer: function(value, metaData, record) {
+						if(record.get("movimentacao") == "EMPRÉSTIMO") {
+							metaData.tdAttr = 'style="background-color:#FF522C;color:#FFFFFF;border-right:8px solid #B22B0D;"';
+							return Ext.Date.format(record.get("data"), 'd/m/Y H:i:s');
+						} else {
+							metaData.tdAttr = 'style="background-color:#8CFF82;color:#000000;"';
+							return Ext.Date.format(record.get("data"), 'd/m/Y H:i:s');
+						}
+					}
                 }
 			],
 			dockedItems: [								
