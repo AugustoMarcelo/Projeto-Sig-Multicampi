6481159aebb9a4f91eb1723cb1b8f8bbb65e0bc0
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-10-10 07:23:20 -0300

    Consertando problema de inconsistência do banco

--- php/protocolo/saveDevolucao.php
+++ php/protocolo/saveDevolucao.php
@@ -3,16 +3,14 @@
 	session_start();
 	//Recuperando os valores enviados pelo formulário
 	$id = $_REQUEST['id'];
-	$tombo = $_REQUEST['tombo'];
-	$denominacao = $_REQUEST['denominacao'];
-	$descricao = $_REQUEST['descricao'];
-	$mediador = $_REQUEST['mediador'];
+	$idPatrimonio = $_REQUEST['id_patrimonio'];
+	$mediador = $_REQUEST['mediadorDevolucao'];
 	$solicitante = $_REQUEST['solicitante'];
-	$dataemprestimo = $_REQUEST['dataemprestimo'];
-	$movimentacao = "EMPRÉSTIMO";
+	$datadevolucao = $_REQUEST['datadevolucao'];
+	$movimentacao = "DEVOLUÇÃO";
 	
-	$insertQuery = "INSERT INTO protocolos (movimentacao, tombo, denominacao, descricao, mediador, solicitante, data) VALUES ('$movimentacao', '$tombo', '$denominacao', '$descricao', '$mediador', '$solicitante', '$dataemprestimo')";
-	$updatePatrimonio = "UPDATE patrimonios SET emprestado = 1 WHERE tombo = '$tombo'";
+	$insertQuery = "INSERT INTO protocolos (movimentacao, id_patrimonio, mediador, solicitante, data) VALUES ('$movimentacao', '$idPatrimonio', '$mediador', '$solicitante', '$datadevolucao')";
+	$updatePatrimonio = "UPDATE patrimonios SET emprestado = 0 WHERE id = '$idPatrimonio'";
 	$resultUpdate = $mysqli->query($updatePatrimonio);
 	if($resultInsert = $mysqli->query($insertQuery)) {
 		$id = $mysqli->insert_id;
