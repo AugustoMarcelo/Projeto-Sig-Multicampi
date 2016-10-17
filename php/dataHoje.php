71e2d547207b460458a1a066f12f165ced31219c
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-10-17 08:41:41 -0300

    Corrigindo problema com horário de verão

--- php/dataHoje.php
+++ php/dataHoje.php
@@ -1,5 +1,5 @@
 <?php
-	date_default_timezone_set("Brazil/East");//SETANDO O FUSO-HORÁRIO
+	date_default_timezone_set("America/Araguaina");//SETANDO O FUSO-HORÁRIO SEM HORÁRIO DE VERÃO
 	$data = date('d/m/Y');
 	$hora = date('H:i:s');
 	echo json_encode(array(
