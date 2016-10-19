61ee2ca435636ae45cce501e4e34708a5ec94bf9
Marcelo Augusto <marcelo_flamengo10@hotmail.com>
2016-10-19 07:52:52 -0300

    VS Code corrigindo espaçamento entre expressões

--- app/controller/Login.js
+++ app/controller/Login.js
@@ -16,13 +16,13 @@ Ext.define('Packt.controller.Login', {
 	],
 
 	refs: [
- 		{
- 			ref: 'capslockTooltip',
- 			selector: 'capslocktooltip'
- 		}
+		{
+			ref: 'capslockTooltip',
+			selector: 'capslocktooltip'
+		}
 	],
 
-	init: function(application) {
+	init: function (application) {
 		this.control({
 			"login form button#submit": {
 				click: this.onButtonClickSubmit
@@ -34,7 +34,7 @@ Ext.define('Packt.controller.Login', {
 				specialkey: this.onTextfieldSpecialKey
 			},
 			"login form textfield[name=password]": {
- 				keypress: this.onTextfieldKeyPress
+				keypress: this.onTextfieldKeyPress
 			},
 			"appheader menuitem#logout": {
 				click: this.onButtonClickLogout
@@ -52,15 +52,15 @@ Ext.define('Packt.controller.Login', {
 		});
 	},
 
-	onButtonClickSubmit: function(button, e, options) {
+	onButtonClickSubmit: function (button, e, options) {
 		var formPanel = button.up('form'),
 			login = button.up('login'),
 			user = formPanel.down('textfield[name=user]').getValue(),
 			pass = formPanel.down('textfield[name=password]').getValue();
 
-		if(formPanel.getForm().isValid()) {
+		if (formPanel.getForm().isValid()) {
 			pass = Packt.util.MD5.encode(pass);
-			Ext.get(login.getEl()).mask("Autenticando... Por favor, espere...",'loading');
+			Ext.get(login.getEl()).mask("Autenticando... Por favor, espere...", 'loading');
 			Ext.Ajax.request({
 				url: 'php/login.php',
 				params: {
@@ -68,87 +68,89 @@ Ext.define('Packt.controller.Login', {
 					password: pass
 				},
 
-				failure: function(conn, response, options, eOpts) {
+				failure: function (conn, response, options, eOpts) {
 					Ext.get(login.getEl()).unmask();
-					Packt.util.Util.decodeJSON(conn.responseText)
+					Packt.util.Util.decodeJSON(conn.responseText);
 				},
 
-				success: function(conn, response, options, eOpts) {
-					Ext.get(login.getEl()).unmask();					
+				success: function (conn, response, options, eOpts) {
+					Ext.get(login.getEl()).unmask();
 					var result = Packt.util.Util.decodeJSON(conn.responseText);
-										
+
 					if (result.success) {
 						login.close();
-					 	Ext.create('Packt.view.MyViewport');
-					 	Packt.util.SessionMonitor.start();
-					 	Ext.Ajax.request({
-					 		method: 'POST',
-					 		url: 'php/security/importUserSession.php',
-					 		success: function(conn, response, options, e0pts) {
-					 			var usuario = Packt.util.Util.decodeJSON(conn.responseText);
-					 			Packt.util.Alert.msg(translations.msgWelcome, usuario.nome);	
-					 		}
-					 	});
-					 	
+						Ext.create('Packt.view.MyViewport');
+						Packt.util.SessionMonitor.start();
+						Ext.Ajax.request({
+							method: 'POST',
+							url: 'php/security/importUserSession.php',
+							success: function (conn, response, options, e0pts) {
+								var usuario = Packt.util.Util.decodeJSON(conn.responseText);
+								Packt.util.Alert.msg(translations.msgWelcome, usuario.nome);
+							}
+						});
 
-					} else {						
-						Packt.util.Util.showErrorMsg(result.msg);
+
+					} else {
+						//Packt.ux.Alert.show('Operação inválida', result.msg, 'error')
+						Packt.util.Alert.msg("Operação inválida", result.msg);
+						//Packt.util.Util.showErrorMsg("Para que você possa acessar, certifique-se de que não há outras sessões abertas neste computador.");
 					}
 				}
 			});
 		}
 	},
 
-	onButtonClickCancel: function(button, e, options) {
-		button.up('form').getForm().reset();	
+	onButtonClickCancel: function (button, e, options) {
+		button.up('form').getForm().reset();
 	},
 
-	onTextfieldSpecialKey: function(field, e, options) {
-		if (e.getKey() == e.ENTER){
+	onTextfieldSpecialKey: function (field, e, options) {
+		if (e.getKey() == e.ENTER) {
 			var submitBtn = field.up('form').down('button#submit');
 			submitBtn.fireEvent('click', submitBtn, e, options);
 		}
 	},
 
-	onTextfieldKeyPress: function(field, e, options) {
- 		var charCode = e.getCharCode(); 
+	onTextfieldKeyPress: function (field, e, options) {
+		var charCode = e.getCharCode();
 
- 		if((e.shiftKey && charCode >= 97 && charCode <= 122) || (!e.shiftKey && charCode >= 65 && charCode <= 90)) {
- 			if(this.getCapslockTooltip() === undefined) { 
- 				Ext.widget('capslocktooltip'); 
- 			}
- 			this.getCapslockTooltip().show(); 
+		if ((e.shiftKey && charCode >= 97 && charCode <= 122) || (!e.shiftKey && charCode >= 65 && charCode <= 90)) {
+			if (this.getCapslockTooltip() === undefined) {
+				Ext.widget('capslocktooltip');
+			}
+			this.getCapslockTooltip().show();
 
- 		} else {
- 			if(this.getCapslockTooltip() !== undefined) { 
- 				this.getCapslockTooltip().hide(); 
- 			}
- 		}	
+		} else {
+			if (this.getCapslockTooltip() !== undefined) {
+				this.getCapslockTooltip().hide();
+			}
+		}
 	},
 
-	onButtonClickLogout: function(button, e, options) {
+	onButtonClickLogout: function (button, e, options) {
 		Ext.Ajax.request({
-			url: 'php/logout.php', 
-			success: function(conn, response, options, eOpts){
-				var result = Packt.util.Util.decodeJSON(conn.responseText);				
-			 	if (result.success) { 
-			 		button.up('mainviewport').destroy(); 
-			 		window.location.reload();
+			url: 'php/logout.php',
+			success: function (conn, response, options, eOpts) {
+				var result = Packt.util.Util.decodeJSON(conn.responseText);
+				if (result.success) {
+					button.up('mainviewport').destroy();
+					window.location.reload();
 				} else {
 					Packt.util.Util.showErrorMsg(conn.responseText);
 				}
 			},
-			failure: function(conn, response, options, eOpts) {
+			failure: function (conn, response, options, eOpts) {
 				Packt.util.Util.showErrorMsg(conn.responseText);
 			}
 		});
 	},
 
-	alterarSenhaView: function(button, e, options) {		
+	alterarSenhaView: function (button, e, options) {
 		Ext.create('Packt.view.security.ChangePasswordView');
 	},
 
-	salvarNovaSenha: function(button, e, options) {
+	salvarNovaSenha: function (button, e, options) {
 		var form = button.up('window').down('form');
 		var novaSenha = form.down('textfield[name=novasenha]').getValue();
 		Ext.Msg.show({
@@ -156,27 +158,27 @@ Ext.define('Packt.controller.Login', {
 			msg: 'Para que os dados sejam completamente homologados, a sua sessão será reiniciada após alterar a nova senha. Deseja continuar?',
 			buttons: Ext.Msg.YESNO,
 			icon: Ext.Msg.WARNING,
-			fn: function(buttonId) {
-				if(buttonId == 'yes') {					
-					if(form.getForm().isValid()) {
-						novaSenha = Packt.util.MD5.encode(novaSenha);			
+			fn: function (buttonId) {
+				if (buttonId == 'yes') {
+					if (form.getForm().isValid()) {
+						novaSenha = Packt.util.MD5.encode(novaSenha);
 						Ext.Ajax.request({
 							url: 'php/security/alterarSenha.php',
 							params: {
 								novasenha: novaSenha
 							},
 
-							failure: function(conn, response, options, eOpts) {					
+							failure: function (conn, response, options, eOpts) {
 								Packt.util.Util.decodeJSON(conn.responseText)
 							},
 
-							success: function(conn, response, options, eOpts) {								
-								var result = Packt.util.Util.decodeJSON(conn.responseText);										
+							success: function (conn, response, options, eOpts) {
+								var result = Packt.util.Util.decodeJSON(conn.responseText);
 								if (result.success) {
 									Packt.util.Alert.msg('Senha alterada com sucesso', '');
 									button.up('window').close();
 									window.location.reload();
-								} else {						
+								} else {
 									Packt.util.Util.showErrorMsg(result.msg);
 								}
 							}
@@ -193,7 +195,7 @@ Ext.define('Packt.controller.Login', {
 		});
 	},
 
-	cancelar: function(button, e, options) {
+	cancelar: function (button, e, options) {
 		button.up('window').close();
 	}
 });
--- app/controller/patrimonio/Patrimonios.js
+++ app/controller/patrimonio/Patrimonios.js
@@ -39,12 +39,12 @@ Ext.define('Packt.controller.patrimonio.Patrimonios', {
 		}
 	],
 
-	init: function(application) {		
+	init: function (application) {
 		this.control({
 			"patrimonioslist": {
 				render: this.onRender,
 				selectionchange: this.onHabilitarBotoes
-			},								
+			},
 			"patrimonios button#addPatrimonio": {
 				click: this.onButtonAddPatrimonio
 			},
@@ -70,75 +70,75 @@ Ext.define('Packt.controller.patrimonio.Patrimonios', {
 				click: this.onButtonFecharWindow
 			}
 		});
-		
-		if(!Ext.getStore('unidades')) {
-            Ext.create('Packt.store.unidade.Unidades');
-        }
+
+		if (!Ext.getStore('unidades')) {
+			Ext.create('Packt.store.unidade.Unidades');
+		}
 	},
 
-	onRender: function(component, options) {
+	onRender: function (component, options) {
 		component.getStore().load();
 	},
 
 	//Função para habilitar os botões de editar e excluir quando o usuário selecionar um registro	
-	onHabilitarBotoes: function(model, selected, e0pts) {		
-		var grid = this.getPatrimonios();					
+	onHabilitarBotoes: function (model, selected, e0pts) {
+		var grid = this.getPatrimonios();
 		var btnDel = grid.down('#delPatrimonio');
 		var btnEdit = grid.down('#editPatrimonio');
 		var btnHist = grid.down('#verHistorico');
-		if(selected[0]) {
-			if(btnDel.isDisabled()) {
+		if (selected[0]) {
+			if (btnDel.isDisabled()) {
 				btnDel.setDisabled(false);
 			}
-			if(btnEdit.isDisabled()) {
+			if (btnEdit.isDisabled()) {
 				btnEdit.setDisabled(false);
 			}
-			if(btnHist.isDisabled()) {
+			if (btnHist.isDisabled()) {
 				btnHist.setDisabled(false);
 			}
 		} else {
-			if(!btnDel.isDisabled()) {
+			if (!btnDel.isDisabled()) {
 				btnDel.setDisabled(true);
 			}
-			if(!btnEdit.isDisabled()) {
+			if (!btnEdit.isDisabled()) {
 				btnEdit.setDisabled(true);
 			}
-			if(!btnHist.isDisabled()) {
+			if (!btnHist.isDisabled()) {
 				btnHist.setDisabled(true);
 			}
 		}
 	},
 
-	onButtonAddPatrimonio: function(button, e, options) {
-		var win = Ext.create('Packt.view.patrimonio.FormPatrimonio');		
+	onButtonAddPatrimonio: function (button, e, options) {
+		var win = Ext.create('Packt.view.patrimonio.FormPatrimonio');
 	},
 
-	onButtonFecharWindow: function(button, e, options) {
+	onButtonFecharWindow: function (button, e, options) {
 		var win = button.up('window');
 		win.close();
 	},
 
-	onButtonSalvarPatrimonio: function(button, e, options) {		
+	onButtonSalvarPatrimonio: function (button, e, options) {
 		var win = button.up('window'),
 			form = win.down('form'),
 			//store = this.getPatrimoniosList().getStore();
-			store = this.getPatrimonioPatrimoniosStore();					
-		if(form.getForm().isValid()) {
+			store = this.getPatrimonioPatrimoniosStore();
+		if (form.getForm().isValid()) {
 			form.getForm().submit({
 				clientValidation: true,
 				url: 'php/patrimonio/savePatrimonio.php',
-				success: function(form, action) {
+				success: function (form, action) {
 					var result = action.result;
-					if(result.success) {
-						Packt.util.Alert.msg('Sucesso!', 'Patrimônio salvo.');										
+					if (result.success) {
+						Packt.util.Alert.msg('Sucesso!', 'Patrimônio salvo.');
 						store.load();
 						win.close();
 					} else {
 						Packt.util.Alert.msg(result.msg);
 					}
 				},
-				failure: function(form, action) {
-					switch(action.failureType) {
+				failure: function (form, action) {
+					switch (action.failureType) {
 						case Ext.form.action.Action.CLIENT_INVALID:
 							Ext.Msg.alert('Erro', 'O formulário pode ter sido preenchido com valores inválidos');
 							break;
@@ -150,10 +150,10 @@ Ext.define('Packt.controller.patrimonio.Patrimonios', {
 					}
 				}
 			});
-		}				
+		}
 	},
 
-	onButtonDeletarPatrimonio: function(button, e, options) {
+	onButtonDeletarPatrimonio: function (button, e, options) {
 		var grid = this.getPatrimoniosList();
 		var record = grid.getSelectionModel().getSelection();
 		var store = grid.getStore();
@@ -163,26 +163,26 @@ Ext.define('Packt.controller.patrimonio.Patrimonios', {
 			msg: 'Deseja mesmo excluir o patrimônio selecionando?',
 			buttons: Ext.Msg.YESNO,
 			icon: Ext.Msg.QUESTION,
-			fn: function(buttonId) {
-				if(buttonId == 'yes') {
+			fn: function (buttonId) {
+				if (buttonId == 'yes') {
 					Ext.Ajax.request({
 						url: 'php/patrimonio/deletarPatrimonio.php',
 						params: {
 							id: record[0].get('id'),
 							imagem: record[0].get('imagem')
 						},
-						success: function(conn, response, options, e0pts) {
-							
+						success: function (conn, response, options, e0pts) {
+
 							var result = Packt.util.Util.decodeJSON(conn.responseText);
 
-							if(result.success) {
+							if (result.success) {
 								Packt.util.Alert.msg('Sucesso', 'Patrimônio excluído');
-								store.load();								
+								store.load();
 							} else {
 								Packt.util.Util.showErrorMsg(conn.responseText);
 							}
 						},
-						failure: function(conn, response, options, e0pts) {
+						failure: function (conn, response, options, e0pts) {
 							Packt.util.Util.showErrorMsg(conn.responseText);
 						}
 					});
@@ -191,39 +191,39 @@ Ext.define('Packt.controller.patrimonio.Patrimonios', {
 		});
 	},
 
-	onFileFieldChange: function(filefield, value, options) {
+	onFileFieldChange: function (filefield, value, options) {
 		var file = filefield.fileInputEl.dom.files[0];
 		var imagem = this.getPatrimonioImagem();
 
-		if(typeof FileReader !== "undefined" && (/image/i).test(file.type)) {
+		if (typeof FileReader !== "undefined" && (/image/i).test(file.type)) {
 			var reader = new FileReader();
-			reader.onload = function(e) {
+			reader.onload = function (e) {
 				imagem.setSrc(e.target.result);
 			};
 			reader.readAsDataURL(file);
-		} else if(!(/image/i).test(file.type)) {
+		} else if (!(/image/i).test(file.type)) {
 			Ext.Msg.alert("Aviso", "Você só pode inserir arquivos de imagens!");
 			filefield.reset();
 		}
 	},
 
-	onButtonEditarPatrimonio: function(button, e, options) {
+	onButtonEditarPatrimonio: function (button, e, options) {
 		var grid = button.up('panel').down('grid');
-		record = grid.getSelectionModel().getSelection();		
-		if(record[0]) {
+		record = grid.getSelectionModel().getSelection();
+		if (record[0]) {
 			var editWindow = this.getFormPatrimonio();
 			//var editWindow = this.getPatrimonioFormPatrimonioView();
-			editWindow.down('form').loadRecord(record[0]);		
-			if(record[0].get('imagem')) {
+			editWindow.down('form').loadRecord(record[0]);
+			if (record[0].get('imagem')) {
 				var img = editWindow.down('image');
-				img.setSrc('resources/patrimonioImages/'+record[0].get('imagem'));
+				img.setSrc('resources/patrimonioImages/' + record[0].get('imagem'));
 			}
 			editWindow.setTitle(record[0].get('denominacao'));
 			editWindow.show();
 		}
 	},
 
-	onButtonVerHistorico: function(button, e, options) {
-		var janela = Ext.create('Packt.view.patrimonio.WindowPatrimonioHistorico');		
+	onButtonVerHistorico: function (button, e, options) {
+		var janela = Ext.create('Packt.view.patrimonio.WindowPatrimonioHistorico');
 	}
 });
